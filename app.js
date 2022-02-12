729069a5d8792d881879d492c5be7c56f4ca1568
Abdul-Rakib92 <rakibctgu@gmail.com>
2022-02-13 02:28:48 +0600

    phone & case increase decrese

--- app.js
+++ app.js
@@ -1,38 +1,54 @@
+/* function updatePhoneNumber(isIncreasing) {
+    const phoneInput = document.getElementById('phone-number');
+    let phoneNumber = phoneInput.value;
+    if(isIncreasing == true){
+    // phoneInput.value = parseInt(phoneNumber) + 1;
+    phoneNumber = parseInt(phoneNumber) + 1;
 
+    }
+    else if(phoneNumber > 0){
+    // phoneInput.value = parseInt(phoneNumber) + 1;
+    phoneNumber = parseInt(phoneNumber) - 1;
+    }
+    phoneInput.value = phoneNumber;
+    //update case total
+    const phoneTotal = document.getElementById('phone-total');
+    phoneTotal.innerText = phoneNumber * 1219;
+}; */
 
 
 // ---
-function updateCaseNumber(isIncreasing) {
-    const caseInput = document.getElementById('case-number');
-    let caseNumber = caseInput.value;
+function updateProductNumber(product, price, isIncreasing) {
+    const productInput = document.getElementById(product + '-number');
+    let productNumber = productInput.value;
     if(isIncreasing == true){
-    // caseInput.value = parseInt(caseNumber) + 1;
-    caseNumber = parseInt(caseNumber) + 1;
+    // productInput.value = parseInt(productNumber) + 1;
+    productNumber = parseInt(productNumber) + 1;
 
     }
-    else if(caseNumber > 0){
-    // caseInput.value = parseInt(caseNumber) - 1;
-    caseNumber = parseInt(caseNumber) - 1;
+    else if(productNumber > 0){
+    // productInput.value = parseInt(productNumber) - 1;
+    productNumber = parseInt(productNumber) - 1;
     }
-    caseInput.value = caseNumber;
-    //update case total
-    const caseTotal = document.getElementById('case-total');
-    caseTotal.innerText = caseNumber * 59;
+    productInput.value = productNumber;
+    //update product total
+    const productTotal = document.getElementById(product + '-total');
+    productTotal.innerText = productNumber * price;
 };
 
-// handle phone increase decrease events
+// phone increase decrease events
 document.getElementById('phone-plus').addEventListener('click', function(){
-    updatePhoneNumber(true);
+    updateProductNumber('phone', 1219, true);
 });
 document.getElementById('phone-minus').addEventListener('click', function(){
-    updatePhoneNumber(false);
+    updateProductNumber('phone', 1219, false);
 });
 
 // handle case increase decrease events
 document.getElementById('case-plus').addEventListener('click', function(){
-    updateCaseNumber(true);
+    updateProductNumber('case', 59, true);
 });
 
 document.getElementById('case-minus').addEventListener('click', function(){
-    updateCaseNumber(false);
+    updateProductNumber('case', 59, false);
 });
