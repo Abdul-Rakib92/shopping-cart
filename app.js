96ce6f36208a3546ac64afbfc5dd26bed9cc8f65
Abdul-Rakib92 <rakibctgu@gmail.com>
2022-02-13 01:53:51 +0600

    phone increse decrease event

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
 
+
+
+// ---
 function updateCaseNumber(isIncreasing) {
     const caseInput = document.getElementById('case-number');
-    const caseNumber = caseInput.value;
+    let caseNumber = caseInput.value;
     if(isIncreasing == true){
-    caseInput.value = parseInt(caseNumber) + 1;
+    // caseInput.value = parseInt(caseNumber) + 1;
+    caseNumber = parseInt(caseNumber) + 1;
 
     }
     else if(caseNumber > 0){
-    caseInput.value = parseInt(caseNumber) - 1;
+    // caseInput.value = parseInt(caseNumber) - 1;
+    caseNumber = parseInt(caseNumber) - 1;
     }
-}
+    caseInput.value = caseNumber;
+    //update case total
+    const caseTotal = document.getElementById('case-total');
+    caseTotal.innerText = caseNumber * 59;
+};
 
+// handle phone increase decrease events
+document.getElementById('phone-plus').addEventListener('click', function(){
+    updatePhoneNumber(true);
+});
+document.getElementById('phone-minus').addEventListener('click', function(){
+    updatePhoneNumber(false);
+});
 
+// handle case increase decrease events
 document.getElementById('case-plus').addEventListener('click', function(){
     updateCaseNumber(true);
 });
